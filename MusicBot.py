e1543ce473967bafc3fd15f958780221cd5ee217
SexualRhinoceros <macw713@gmail.com>
2015-12-30 01:17:25 -0500

    another 1 line code change

--- MusicBot.py
+++ MusicBot.py
@@ -126,7 +126,7 @@ def on_message(message):
             count = 0
             for servers in client.servers:
                 count += 1
-            if count > 1:
+            if count > 1 and client.user.id != '127715185115791363':
                 yield from client.send_message(message.channel,'I DIDN\'T LISTEN TO DIRECTIONS AND HAVE MY BOT ON MORE THAN ONE SERVER')
             else:
                 print('you good')
