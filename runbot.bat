e93fb0e6a6c938db5c41ccde0c7649e5cb92ae99
SexualRhinoceros <macw713@gmail.com>
2015-12-31 21:15:29 -0500

    now checks for existance of 3.5.1 and uses it

--- runbot.bat
+++ runbot.bat
@@ -1,2 +1,29 @@
-python MusicBot.py
+@echo off
+SETLOCAL EnableDelayedExpansion
+SET version=1
+cls
+FOR /f "delims=" %%a IN ('where python') DO (
+    FOR /F "tokens=* USEBACKQ" %%F IN (`"%%a" -V`) DO (
+	    cls
+        SET var=%%F
+    )
+    SET var=!var:~7,5!
+    IF !var! GTR !version! (
+        SET version=!var!
+        SET pypath="%%a"
+    )
+)
+IF /I NOT %version:~0,3% == 3.5 GOTO errorhandler
+%pypath% run.py
+GOTO end
+
+:errorhandler
+IF /I %version% == 0 GOTO nopython
+ECHO ERROR: Bad version detected at %var%, please install Python 3.5.1
+GOTO end
+
+:nopython
+ECHO ERROR: No install of Python has been detected. Please review the README for more information
+
+:end
 pause
