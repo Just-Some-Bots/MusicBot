f142dde083e200296977b9f7cd8123025f32bb00
SexualRhinoceros <macw713@gmail.com>
2015-12-31 21:27:36 -0500

    Added Git check as well

--- runbot.bat
+++ runbot.bat
@@ -1,6 +1,8 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 SET version=1
+FOR /F "tokens=* USEBACKQ" %%H IN (`git --version`) DO SET gvar=%%F
+IF /I NOT %gvar:~0,6% == git ve GOTO nogit
 cls
 FOR /f "delims=" %%a IN ('where python') DO (
     FOR /F "tokens=* USEBACKQ" %%F IN (`"%%a" -V`) DO (
@@ -17,9 +19,13 @@ IF /I NOT %version:~0,3% == 3.5 GOTO errorhandler
 %pypath% run.py
 GOTO end
 
-:errorhandler
+:errorhandlerpy
 IF /I %version% == 0 GOTO nopython
 ECHO ERROR: Bad version detected at %var%, please install Python 3.5.1
+GOTO end
+
+:nogit
+ECHO ERROR: Git Installation not detected. Please review the README for more information
 GOTO end
 
 :nopython
