9cb1978bf05597090b302822e919c28aaaf32a52
Daniel <dalmodovar.1337@gmail.com>
2016-01-05 16:17:00 -0500

    comment

--- fixnupdate.bat
+++ fixnupdate.bat
@@ -1,3 +1,4 @@
+rem We might still have the same problem with using "python" as the command, i.e. py -3, no python 3, etc
 python -m pip install --upgrade git+https://github.com/Rapptz/discord.py@async
 python -m pip install --upgrade youtube_dl
 pause
