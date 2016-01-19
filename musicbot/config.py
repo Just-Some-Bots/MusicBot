5dea392681ae4bbb9de4491a87052140f7e9774e
Neddz <fabiomoyano22@gmail.com>
2016-01-19 16:38:32 -0500

    Get PermissionGroups from Config, raise ValueError in case there isn't a default group.

--- musicbot/config.py
+++ musicbot/config.py
@@ -10,6 +10,10 @@ class Config(object):
         self.password = config.get('Credentials', 'Password', fallback=None)
 
         self.owner_id = config.get('Permissions', 'OwnerID', fallback=None)
+        self.permission_groups = dict()
+        for group in config['PermissionGroups']:
+            self.permission_groups[group.lower()] = config.get('PermissionGroups', group, fallback=None).lower().split()
+
         self.command_prefix = config.get('Chat', 'CommandPrefix', fallback='!')
 
         self.days_active = config.getint('MusicBot', 'DaysActive', fallback=0)
@@ -30,6 +34,9 @@ class Config(object):
 
         if not self.owner_id:
             raise ValueError("An owner is not specified in the configuration file")
+
+        if 'default' not in self.permission_groups:
+            raise ValueError("The default permission group should be defined in the configuration file.")
 
     # TODO: Add save function for future editing of options with commands
     #       Maybe add warnings about fields missing from the config file
