fc198f61d3cdc45f1f4b9980414c2d582751fe41
Daniel <dalmodovar.1337@gmail.com>
2016-01-21 01:50:17 -0500

    Added song progress and fixed queue bug

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -21,6 +21,7 @@ from .constants import DISCORD_MSG_CHAR_LIMIT
 from .opus_loader import load_opus_lib
 
 from random import choice
+from datetime import timedelta
 
 VERSION = '2.0'
 
@@ -604,17 +605,25 @@ class MusicBot(discord.Client):
         andmoretext = '* ... and %s more*' % ('x'*len(player.playlist.entries))
 
         if player.current_entry:
+            song_progress = str(timedelta(seconds=player.progress)).lstrip('0').lstrip(':')
+            song_total = str(timedelta(seconds=player.current_entry.duration)).lstrip('0').lstrip(':')
+            prog_str = '`[%s/%s]`' % (song_progress, song_total)
+
             if player.current_entry.meta.get('channel', False) and player.current_entry.meta.get('author', False):
-                lines.append("Now Playing: **%s** added by **%s**\n" % (
-                    player.current_entry.title, player.current_entry.meta['author'].name))
+                lines.append("Now Playing: **%s** added by **%s** %s\n" % (
+                    player.current_entry.title, player.current_entry.meta['author'].name, prog_str))
             else:
-                lines.append("Now Playing: **%s**\n" % player.current_entry.title)
+                lines.append("Now Playing: **%s** %s\n" % (player.current_entry.title, prog_str))
+
 
         for i, item in enumerate(player.playlist, 1):
-            nextline = '`{}.` **{}** added by **{}**'.format(i, item.title, item.meta['author'].name).strip()
+            if item.meta.get('channel', False) and item.meta.get('author', False):
+                nextline = '`{}.` **{}** added by **{}**'.format(i, item.title, item.meta['author'].name).strip()
+            else:
+                nextline = '`{}.` **{}**'.format(i, item.title).strip()
+
             currentlinesum = sum([len(x)+1 for x in lines]) # +1 is for newline char
 
-            # This is fine I guess, don't need to worry too much about trying to squeeze as much in as possible
             if currentlinesum + len(nextline) + len(andmoretext) > DISCORD_MSG_CHAR_LIMIT:
                 if currentlinesum + len(andmoretext):
                     unlisted += 1
