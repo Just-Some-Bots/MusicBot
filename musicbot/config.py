353f1aa60256cea797e491996b5d681f7b440c5c
Daniel <dalmodovar.1337@gmail.com>
2016-02-01 00:06:13 -0500

    Added ConfigDefaults class

--- musicbot/config.py
+++ musicbot/config.py
@@ -1,29 +1,53 @@
 import configparser
 
 
+class ConfigDefaults(object):
+    username = None
+    password = None
+
+    owner_id = None
+    command_prefix = '!'
+
+    days_active = 0 # I might remove this entirely, it seems kinda dumb now that there's server verification levels
+    white_list_check = False
+    skips_required = 5
+    skip_ratio_required = 0.5
+    save_videos = True
+    now_playing_mentions = False
+    auto_summon = True
+    auto_playlist = True
+
+    blacklist_file = 'config/blacklist.txt'
+    whitelist_file = 'config/whitelist.txt'
+    backup_playlist_file = 'config/backuplist.txt' # this will change when I add playlists
+
+
 class Config(object):
     def __init__(self, config_file):
         config = configparser.ConfigParser()
         config.read(config_file)
 
-        self.username = config.get('Credentials', 'Username', fallback=None)
-        self.password = config.get('Credentials', 'Password', fallback=None)
+        # Maybe wrap these in a helper and change ConfigDefaults names to their config value
 
-        self.owner_id = config.get('Permissions', 'OwnerID', fallback=None)
-        self.command_prefix = config.get('Chat', 'CommandPrefix', fallback='!')
+        self.username = config.get('Credentials', 'Username', fallback=ConfigDefaults.username)
+        self.password = config.get('Credentials', 'Password', fallback=ConfigDefaults.password)
 
-        self.days_active = config.getint('MusicBot', 'DaysActive', fallback=0)
-        self.white_list_check = config.getboolean('MusicBot', 'WhiteListCheck', fallback=False)
-        self.skips_required = config.getint('MusicBot', 'SkipsRequired', fallback=7)
-        self.skip_ratio_required = config.getfloat('MusicBot', 'SkipRatio', fallback=0.5)
-        self.save_videos = config.getboolean('MusicBot', 'SaveVideos', fallback=True)
-        self.now_playing_mentions = config.getboolean('MusicBot', 'NowPlayingMentions', fallback=True)
-        self.auto_summon = config.getboolean('MusicBot', 'AutoSummon', fallback=True)
-        self.auto_playlist = config.getboolean('MusicBot', 'UseAutoPlaylist', fallback=True)
+        self.owner_id = config.get('Permissions', 'OwnerID', fallback=ConfigDefaults.owner_id)
+        self.command_prefix = config.get('Chat', 'CommandPrefix', fallback=ConfigDefaults.command_prefix)
 
-        self.blacklist_file = config.get('Files', 'BlacklistFile', fallback='config/blacklist.txt')
-        self.whitelist_file = config.get('Files', 'WhitelistFile', fallback='config/whitelist.txt')
-        self.backup_playlist_file = config.get('Files', 'BlackupPlaylistFile', fallback='config/backuplist.txt')
+        self.days_active = config.getint('MusicBot', 'DaysActive', fallback=ConfigDefaults.days_active)
+        self.white_list_check = config.getboolean('MusicBot', 'WhiteListCheck', fallback=ConfigDefaults.white_list_check)
+        self.skips_required = config.getint('MusicBot', 'SkipsRequired', fallback=ConfigDefaults.skips_required)
+        self.skip_ratio_required = config.getfloat('MusicBot', 'SkipRatio', fallback=ConfigDefaults.skip_ratio_required)
+        self.save_videos = config.getboolean('MusicBot', 'SaveVideos', fallback=ConfigDefaults.save_videos)
+        self.now_playing_mentions = config.getboolean('MusicBot', 'NowPlayingMentions', fallback=ConfigDefaults.now_playing_mentions)
+        self.auto_summon = config.getboolean('MusicBot', 'AutoSummon', fallback=ConfigDefaults.auto_summon)
+        self.auto_playlist = config.getboolean('MusicBot', 'UseAutoPlaylist', fallback=ConfigDefaults.auto_playlist)
+
+        self.blacklist_file = config.get('Files', 'BlacklistFile', fallback=ConfigDefaults.blacklist_file)
+        self.whitelist_file = config.get('Files', 'WhitelistFile', fallback=ConfigDefaults.whitelist_file)
+        self.backup_playlist_file = config.get('Files', 'BlackupPlaylistFile', fallback=ConfigDefaults.backup_playlist_file)
+
 
         # Validation logic for bot settings.
         if not self.username or not self.password:
