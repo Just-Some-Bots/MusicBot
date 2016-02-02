539f4c96c5e34ee0be7fba508a4a5d6b42581b30
Daniel <dalmodovar.1337@gmail.com>
2016-02-02 02:54:00 -0500

    I hope this fixes the ipv6 issue

--- musicbot/downloader.py
+++ musicbot/downloader.py
@@ -12,7 +12,8 @@ ytdl_format_options = {
     'nocheckcertificate': True,
     'ignoreerrors': True,
     'quiet': True,
-    'no_warnings': True
+    'no_warnings': True,
+    'source_address': '0.0.0.0' # This is evidently an experimental youtube-dl feature
 }
 
 thread_pool = ThreadPoolExecutor(max_workers=2)
