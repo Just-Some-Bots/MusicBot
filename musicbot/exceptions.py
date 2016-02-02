a361338e6e82ce7e13f6e8c5efeb790e72b8286c
Daniel <dalmodovar.1337@gmail.com>
2016-02-02 02:55:41 -0500

    Not done, but maybe we'll get fewer dumb questions

--- musicbot/exceptions.py
+++ musicbot/exceptions.py
@@ -1,10 +1,21 @@
 class CommandError(Exception):
     def __init__(self, message):
-        self.message = message
-        super().__init__()
+        super().__init__(message)
 
 
 class ExtractionError(Exception):
     def __init__(self, message):
-        self.message = message
-        super().__init__()
+        super().__init__(message)
+
+class HelpfulError(Exception):
+    def __init__(self, issue, solution):
+        self.issue = issue
+        self.solution = solution
+        super().__init__(self._construct_msg())
+
+    def _construct_msg(self):
+        return ("\n"
+            "An error has occured.\n"
+            "  Cause: {}\n"
+            "  Solution: {}\n"
+            ).format(self.issue, self.solution)
