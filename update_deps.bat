de021ad1513f53738c0d90fcfcaa58f5e12e564f
Daniel <dalmodovar.1337@gmail.com>
2016-02-10 20:36:27 -0500

    I forgot to change this one

--- update_deps.bat
+++ update_deps.bat
@@ -1,45 +1,21 @@
 @ECHO off
-SETLOCAL EnableDelayedExpansion
 
+CHCP 65001 > NUL
 CD /d "%~dp0"
 
-SET version=1
-FOR /F "tokens=* USEBACKQ" %%H IN (`git --version`) DO SET gvar=%%F
-IF /I NOT %gvar:~0,6% == git ve GOTO nogit
-CLS
-
-IF EXIST C:\Windows\py.exe (
-	CMD /k C:\Windows\py.exe -3.5 -m pip install --upgrade -r requirements.txt
-    GOTO end
+IF EXIST %SYSTEMROOT%\py.exe (
+    CMD /k C:\Windows\py.exe -3.5 -m pip install --upgrade -r requirements.txt
+    EXIT
 )
 
-FOR /f "delims=" %%a IN ('C:\Windows\System32\where.exe python') DO (
-    FOR /F "tokens=* USEBACKQ" %%F IN (`"%%a" -V`) DO (
-	    CLS
-        SET var=%%F
-    )
-    SET var=!var:~7,5!
-    IF !var! GTR !version! (
-        SET version=!var!
-        SET pypath="%%a"
-    )
-)
+python --version > NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 GOTO nopython
 
-IF /I NOT %version:~0,3% == 3.5 GOTO errorhandlerpy
-CMD /k %pypath% -m pip install --upgrade -r requirements.txt
-GOTO end
-
-:errorhandlerpy
-IF /I %version% == 0 GOTO nopython
-ECHO ERROR: Bad version detected at %var%, please install Python 3.5+
-GOTO end
-
-:nogit
-ECHO ERROR: Git Installation not detected. Git is required to install dependencies.
+CMD /k python -m pip install --upgrade -r requirements.txt
 GOTO end
 
 :nopython
-ECHO ERROR: No install of Python has been detected. Python 3.5+ is required.
+ECHO ERROR: Git has either not been installed or not added to your PATH.
 
 :end
 PAUSE
