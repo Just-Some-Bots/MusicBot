850731567602a920d926ef57ac36de15e5b76ab8
Daniel <dalmodovar.1337@gmail.com>
2016-02-14 21:53:29 -0500

    Added expire_in argument for errors
    Except ExtractionError, it doesn't need one

--- musicbot/exceptions.py
+++ musicbot/exceptions.py
@@ -1,16 +1,18 @@
 class CommandError(Exception):
-    def __init__(self, message):
+    def __init__(self, message, expire_in=0):
         self.message = message
+        self.expire_in = expire_in
 
 class ExtractionError(Exception):
     def __init__(self, message):
         self.message = message
 
 class HelpfulError(Exception):
-    def __init__(self, issue, solution):
+    def __init__(self, issue, solution, expire_in=0):
         self.issue = issue
         self.solution = solution
-        super().__init__(self._construct_msg())
+        self.expire_in = expire_in
+        self.message = self._construct_msg()
 
     def _construct_msg(self):
         return ("\n"
