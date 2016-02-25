14f59725448ee616c2a1d7958bc07aa7bd574f24
Daniel <dalmodovar.1337@gmail.com>
2016-02-25 01:54:16 -0500

    Fixed permissions related bugs

--- musicbot/permissions.py
+++ musicbot/permissions.py
@@ -46,7 +46,7 @@ class Permissions:
                 return group
 
         # The only way I could search for roles is if I add a `server=None` param and pass that too
-        if isinstance(user, discord_User):
+        if type(user) == discord_User:
             return self.default_group
 
         # We loop again so that we don't return a role based group before we find an assigned one
