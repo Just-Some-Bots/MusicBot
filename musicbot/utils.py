8bc9237e5a9d3cc30bb66ad793ba9d2945f698b7
Daniel <dalmodovar.1337@gmail.com>
2016-04-20 13:00:52 -0400

    Added pagination helper function

--- musicbot/utils.py
+++ musicbot/utils.py
@@ -2,6 +2,8 @@ import re
 import decimal
 import unicodedata
 
+from .constants import DISCORD_MSG_CHAR_LIMIT
+
 _USER_ID_MATCH = re.compile(r'<@(\d+)>')
 
 
@@ -43,3 +45,30 @@ def slugify(value):
 
 def sane_round_int(x):
     return int(decimal.Decimal(x).quantize(1, rounding=decimal.ROUND_HALF_UP))
+
+
+def paginate(content, *, length=DISCORD_MSG_CHAR_LIMIT, reserve=0):
+    """
+    Split up a large string or list of strings into chunks for sending to discord.
+    """
+    if type(content) == str:
+        contentlist = content.split('\n')
+    elif type(content) == list:
+        contentlist = content
+    else:
+        raise ValueError("Content must be str or list, not %s" % type(content))
+
+    chunks = []
+    currentchunk = ''
+
+    for line in contentlist:
+        if len(currentchunk) + len(line) < length - reserve:
+            currentchunk += line + '\n'
+        else:
+            chunks.append(currentchunk)
+            currentchunk = ''
+
+    if currentchunk:
+        chunks.append(currentchunk)
+
+    return chunks
