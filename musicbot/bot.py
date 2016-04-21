c4048ded87ed8059a0dcb0cea6dc568fa33dcdb2
gfrewqpoiu <kevinstriek@KevinMac.local>
2016-04-21 11:20:31 +0200

    Implemented NextSong Command

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -1234,7 +1234,7 @@ class MusicBot(discord.Client):
 
         if player.current_entry.meta.get('channel', False) and player.current_entry.meta.get('author', False):
             player.current_entry.meta.get('author', False)
-            if author.id == player.current_entry.meta['author'].id:
+            if author.id == player.current_entry.meta['author'].id: #If person that requested the song skips, skip instantly
                 player.skip()  # check autopause stuff here
                 return
 
@@ -1346,6 +1346,52 @@ class MusicBot(discord.Client):
 
             if currentlinesum + len(nextline) + len(andmoretext) > DISCORD_MSG_CHAR_LIMIT:
                 if currentlinesum + len(andmoretext):
+                    unlisted += 1
+                    continue
+
+            lines.append(nextline)
+
+        if unlisted:
+            lines.append('\n*... and %s more*' % unlisted)
+
+        if not lines:
+            lines.append(
+                'There are no songs queued! Queue something with {}play.'.format(self.config.command_prefix))
+
+        message = '\n'.join(lines)
+        return Response(message, delete_after=30)
+        
+   async def cmd_nextsong(self, channel, player):
+        """
+        Usage:
+            {command_prefix}nextsong
+
+        Prints the next song in the queue.
+        """
+
+        lines = []
+        unlisted = 0
+        andmoretext = '* ... and %s more*' % ('x' * len(player.playlist.entries))
+
+        if player.current_entry:
+            song_progress = str(timedelta(seconds=player.progress)).lstrip('0').lstrip(':')
+            song_total = str(timedelta(seconds=player.current_entry.duration)).lstrip('0').lstrip(':')
+            prog_str = '`[%s/%s]`' % (song_progress, song_total)
+
+            if player.current_entry.meta.get('channel', False) and player.current_entry.meta.get('author', False):
+                lines.append("Now Playing: **%s** added by **%s** %s\n" % (
+                    player.current_entry.title, player.current_entry.meta['author'].name, prog_str))
+            else:
+                lines.append("Now Playing: **%s** %s\n" % (player.current_entry.title, prog_str))
+
+            if item.meta.get('channel', False) and item.meta.get('author', False):
+                nextline = '`{}.` **{}** added by **{}**'.format(i, item.title, item.meta['author'].name).strip()
+            else:
+                nextline = '`{}.` **{}**'.format(i, item.title).strip()
+
+            currentlinesum = sum([len(x) + 1 for x in lines])  # +1 is for newline char
+
+            if currentlinesum + len(andmoretext):
                     unlisted += 1
                     continue
 
