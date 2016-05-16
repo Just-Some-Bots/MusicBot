88b8a6977bb0985cb0bddae2f925eed80d279a25
Daniel <dalmodovar.1337@gmail.com>
2016-05-15 21:26:04 -0400

    Added more bot version information

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,6 +1,8 @@
 import os.path
 
-VERSION = '1.9.5'
+MAIN_VERSION = '1.9.5'
+SUB_VERSION = '-review-2016.5.15'
+VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
 DISCORD_MSG_CHAR_LIMIT = 2000
