b86597fccea1af66cfa44a64c275c3051b0809f8
Daniel <imayhaveborkedit@users.noreply.github.com>
2016-05-22 18:19:54 -0400

    change version

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
 MAIN_VERSION = '1.9.5'
-SUB_VERSION = '-review-2016.5.19'
+SUB_VERSION = ''
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
