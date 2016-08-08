04d252a33a0d4fb93401cfb0f41264244b47a9ff
Derek Maciel <derekamaciel@gmail.com>
2016-08-08 00:23:56 -0400

    Remove redundant SUB_VERSION constant
    We're not using it anyway, and this makes it a bit easier to check for
    updates

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,8 +1,6 @@
 import os.path
 
-MAIN_VERSION = '1.9.5_2'
-SUB_VERSION = ''
-VERSION = MAIN_VERSION + SUB_VERSION
+VERSION = '1.9.5_2'
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
 DISCORD_MSG_CHAR_LIMIT = 2000
