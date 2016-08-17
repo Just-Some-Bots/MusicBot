f8d1bef594ce8225e33017396686976166e311e6
Artanys <Jordan.naumann@gmail.com>
2016-08-17 15:46:25 -0700

    adding del fuc with session closing

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -95,6 +95,13 @@ class MusicBot(discord.Client):
         self.aiosession = aiohttp.ClientSession(loop=self.loop)
         self.http.user_agent += ' MusicBot/%s' % BOTVERSION
 
+    def __del__(self):
+        try:
+            if not self.http.session.closed:
+                self.http.session.close()
+        except:
+            pass
+
     # TODO: Add some sort of `denied` argument for a message to send when someone else tries to use it
     def owner_only(func):
         @wraps(func)
