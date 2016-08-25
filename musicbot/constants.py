28f619eec533ff20ef7ae445e3a0469df28f0cfd
Daniel <dalmodovar.1337@gmail.com>
2016-08-25 03:11:17 -0400

    Bump version
    Don't think we're done yet, oh no.

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
-MAIN_VERSION = '1.9.5_2'
-SUB_VERSION = ''
+MAIN_VERSION = '1.9.6'
+SUB_VERSION = '-review'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
