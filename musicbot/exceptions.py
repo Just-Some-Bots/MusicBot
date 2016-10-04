57d9bc105622193dd7a35f829a38110d063624ec
Daniel <imayhaveborkedit@gmail.com>
2016-10-03 22:53:55 -0400

    Update HelpfulError format, added footnote arg
    Rip nice formatting

--- musicbot/exceptions.py
+++ musicbot/exceptions.py
@@ -48,39 +48,44 @@ class PermissionsError(CommandError):
 
 # Error with pretty formatting for hand-holding users through various errors
 class HelpfulError(MusicbotException):
-    def __init__(self, issue, solution, *, preface="An error has occured:\n", expire_in=0):
+    def __init__(self, issue, solution, *, preface="An error has occured:", footnote='', expire_in=0):
         self.issue = issue
         self.solution = solution
         self.preface = preface
+        self.footnote = footnote
         self.expire_in = expire_in
+        self._message_fmt = "\n{preface}\n{problem}\n\n{solution}\n\n{footnote}"
 
     @property
     def message(self):
-        return ("\n{}\n{}\n{}\n").format(
-            self.preface,
-            self._pretty_wrap(self.issue,    "  Problem:  "),
-            self._pretty_wrap(self.solution, "  Solution: "))
+        return self._message_fmt.format(
+            preface  = self.preface,
+            problem  = self._pretty_wrap(self.issue,    "  Problem:"),
+            solution = self._pretty_wrap(self.solution, "  Solution:"),
+            footnote = self.footnote
+        )
 
     @property
     def message_no_format(self):
-        return "\n{}\n{}\n{}\n".format(
-            self.preface,
-            self._pretty_wrap(self.issue,    "  Problem:  ", width=None),
-            self._pretty_wrap(self.solution, "  Solution: ", width=None))
+        return self._message_fmt.format(
+            preface  = self.preface,
+            problem  = self._pretty_wrap(self.issue,    "  Problem:", width=None),
+            solution = self._pretty_wrap(self.solution, "  Solution:", width=None),
+            footnote = self.footnote
+        )
 
     @staticmethod
     def _pretty_wrap(text, pretext, *, width=-1):
         if width is None:
-            return pretext + text
+            return '\n'.join((pretext.strip(), text))
         elif width == -1:
+            pretext = pretext.rstrip() + '\n'
             width = shutil.get_terminal_size().columns
 
-        l1, *lx = textwrap.wrap(text, width=width - 1 - len(pretext))
+        lines = textwrap.wrap(text, width=width - 5)
+        lines = (('    ' + line).rstrip().ljust(width-1).rstrip() + '\n' for line in lines)
 
-        lx = [((' ' * len(pretext)) + l).rstrip().ljust(width) for l in lx]
-        l1 = (pretext + l1).ljust(width)
-
-        return ''.join([l1, *lx])
+        return pretext + ''.join(lines).rstrip()
 
 class HelpfulWarning(HelpfulError):
     pass
