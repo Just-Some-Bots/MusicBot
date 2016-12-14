77c4b280b2742a99b45e6e33e6b1b5ddffe47c07
KingGold171 <kinggold171@gmail.com>
2016-12-14 00:24:39 +0000

    Added Spanish
    Just to test and prove it works based on selected language from
    options.ini

---
+++ languages/spanish.py
@@ -0,0 +1,3 @@
+no_autoplaylist = "Advertencia: la lista de reproducción automática está vacía, inhabilitando."
+autosummon_attempt = "Intentando autosummon..."
+autosummon_found_owner = "Se encuentra dueño en \"%s\", Intentando unirse..."
