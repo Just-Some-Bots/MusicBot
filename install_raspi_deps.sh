0460ed15c70907109fe7f4a4e0083eca835d353b
Pedro Portela <pedro12909@hotmail.com>
2017-01-31 15:56:24 +0000

    Added sh file to install deps on a raspi

---
+++ install_raspi_deps.sh
@@ -0,0 +1,14 @@
+apt-get install sudo
+apt-get install git
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade
+sudo apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev
+sudo apt-get install zlib1g-dev libsqlite3-dev tk-dev
+sudo apt-get install libssl-dev openssl
+sudo apt-get install build-essential unzip -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:fkrull/deadsnakes -y
+sudo add-apt-repository ppa:mc3man/trusty-media -y
+sudo add-apt-repository ppa:chris-lea/libsodium -y
+sudo apt-get install libopus-dev libffi-dev libsodium-dev
