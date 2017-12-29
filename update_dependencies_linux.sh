c6f862b567cbd5fe79527a93d45b3caa4d092892
jadissa <jadissag@gmail.com>
2017-12-29 01:58:29 -0800

    Added missing dependencies for CentOS7

--- update_dependencies_linux.sh
+++ update_dependencies_linux.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-python3.5 -V > /dev/null 2>&1 || {
-    echo >&2 "Python 3.5 doesn't seem to be installed.  Do you have a weird installation?"
-    echo >&2 "If you have python 3.5, use it to run run.py instead of this script."
-    exit 1; }
-
-python3.5 -m pip install --upgrade -r requirements.txt
+sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+sudo yum -y update
+sudo yum -y install libffi.x86_64 libffi-devel.x86_64
+sudo yum -y install opus-devel.x86_64 opus-tools.x86_64 opus.x86_64 
+sudo yum -y install ffmpeg ffmpeg-devel
+python3.5 -V > /dev/null 2>&1 || sudo yum install -y python35u python35u-libs python35u-devel python35u-pip
+sudo /usr/bin/python3.5 run.py
