7f19cf82ca45db5dce9fde4b1e8047193e175dc0
Rhino <rhino@notmyrealemail.com>
2018-01-14 18:48:04 -0800

    Remove the old remove command
    lul didnt notice it. It doesnt have any tied in permissions making it
    more prone to abuse so should be a nice boost for anyone who likes the
    command

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2206,37 +2206,7 @@ class MusicBot(discord.Client):
                 reply=True,
                 delete_after=20
             )
-
-    async def cmd_remove(self, message, player, index):
-        """
-        Usage:
-            {command_prefix}remove [number]
-	    
-        Removes a song from the queue at the given position, where the position is a number from {command_prefix}queue.
-        """
-
-        if not player.playlist.entries:
-            raise exceptions.CommandError("There are no songs queued.", expire_in=20)
-
-        try:
-            index = int(index)
-
-        except ValueError:
-            raise exceptions.CommandError('{} is not a valid number.'.format(index), expire_in=20)
-
-        if 0 < index <= len(player.playlist.entries):
-            try:
-                song_title = player.playlist.entries[index-1].title
-                player.playlist.remove_entry((index)-1)
-
-            except IndexError:
-                raise exceptions.CommandError("Something went wrong while the song was being removed. Try again with a new position from `" + self.config.command_prefix + "queue`", expire_in=20)
-
-            return Response("\N{CHECK MARK} removed **" + song_title + "**", delete_after=20)
-
-        else:
-            raise exceptions.CommandError("You can't remove the current song (skip it instead), or a song in a position that doesn't exist.", expire_in=20)
-
+            
     async def cmd_volume(self, message, player, new_volume=None):
         """
         Usage:
