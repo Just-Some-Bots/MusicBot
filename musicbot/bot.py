7619896b11df08694b4d0fc7d2f3a56d0bb92b1a
Jayden Bailey <jaydenkieran@gmail.com>
2018-02-15 19:27:30 +0000

    Don't count the bot when assessing the skip reqs

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -2155,7 +2155,7 @@ class MusicBot(discord.Client):
         # Currently is recounted if they vote, deafen, then vote
 
         num_voice = sum(1 for m in voice_channel.voice_members if not (
-            m.deaf or m.self_deaf))
+            m.deaf or m.self_deaf or m == self.user))
 
         num_skips = player.skip_state.add_skipper(author.id, message)
 
