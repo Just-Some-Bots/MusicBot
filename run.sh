38520ff0cf0b27e5b87948f51979849b6e41e75f
Conner May <aleanaazure@gmail.com>
2018-04-25 03:18:52 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(command -v python3 >/dev/null 2>&1 &&
+(command -v python3.5 >/dev/null 2>&1 &&
 python3.5 -c 'import sys; sys.exit(sys.hexversion < 0x03050000)') || {
 	echo >&2 "Python 3.5 or later not found."
 	echo >&2 "If you have python, use it to run run.py."
