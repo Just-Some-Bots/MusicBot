8fbb4a82af161c3122007dbadf806ef4217556f1
Conner May <aleanaazure@gmail.com>
2018-05-08 19:05:16 -0400

    py3.6 command addition.

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ PYTHON_VERSION_1=`python -c 'import sys; version=sys.version_info[:3]; print("{0
 PYTHON_VERSION_2=`python -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))'`
 PYTHON3_VERSION=`python3 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))'`
 PYTHON35_VERSION=`python3.5 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))'`
+PYTHON36_VERSION=`python3.6 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))'`
 
 # Check if the python command is python 3.5 or greater
 if [ "$PYTHON_VERSION_1" -eq "3" ]; then
@@ -19,6 +20,8 @@ elif [ "$PYTHON3_VERSION" -ge "5" ]; then
 # doesn't attempt to run something that doesn't exist
 elif [ "$PYTHON35_VERSION" -eq "5" ]; then
     python3.5 update.py
+elif [ "$PYTHON36_VERSION" -eq "6" ]; then
+    python3.6 run.py
 else
     echo "Your version of Python is lower than 3.5, please install a more recent version."
 fi
