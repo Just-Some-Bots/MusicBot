4a3c02adcad8e392ee8a03891289a5d1ab0ed3f5
Jayden Bailey <jaydenkieran@users.noreply.github.com>
2018-08-02 23:31:55 +0100

    Fix travis builds

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,8 +6,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install opus
     brew install libffi
     brew install libsodium  
-    python3 -m pip install -U pip
-    python3 -m pip install -U -r requirements.txt
+    python -m pip install -U pip
+    python -m pip install -U -r requirements.txt
 else
     sudo apt-get install build-essential unzip -y
     sudo apt-get install software-properties-common -y
@@ -17,6 +17,6 @@ else
     sudo apt-get update -y
     sudo apt-get install git python3.5-dev libav-tools libopus-dev libffi-dev libsodium-dev python3-pip -y
     sudo apt-get upgrade -y
-    sudo python3 -m pip install -U pip
-    sudo python3 -m pip install -U -r requirements.txt
+    sudo python -m pip install -U pip
+    sudo python -m pip install -U -r requirements.txt
 fi
