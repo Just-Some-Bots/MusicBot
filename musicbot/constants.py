04ccf9569856b1300a1fd27573a3d6479aa1d8cc
AmiT177 <8685831+AmiT177@users.noreply.github.com>
2018-08-11 22:23:12 +0300

    Updated sub version

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
 MAIN_VERSION = '1.9.8'
-SUB_VERSION = '-300618-review'
+SUB_VERSION = '-110818-review'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
