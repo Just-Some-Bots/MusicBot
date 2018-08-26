1f22ae7e791646bfbbd050f89cad7317aed70727
Conner May <aleanaazure@gmail.com>
2018-08-26 05:09:56 -0400

    *fully* testing a script is good to do.

--- run.sh
+++ run.sh
@@ -3,25 +3,46 @@
 # Set variables for python versions. Could probably be done cleaner, but this works.
 PYTHON_VERSION_1=`python -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[0]))' || { echo "no py"; }`
 PYTHON_VERSION_2=`python -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))' || { echo "no py"; }`
-PYTHON3_VERSION=`python3 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))' || { echo "no py3"; }`
-PYTHON35_VERSION=`python3.5 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))' || { echo "no py35"; }`
+PYTHON_VERSION_3=`python -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[2]))' || { echo "no py"; }`
+PYTHON3_VERSION_1=`python3 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))' || { echo "no py3"; }`
+PYTHON3_VERSION_2=`python3 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[2]))' || { echo "no py3"; }`
+PYTHON35_VERSION_1=`python3.5 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))' || { echo "no py35"; }`
+PYTHON35_VERSION_2=`python3.5 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[2]))' || { echo "no py35"; }`
 PYTHON36_VERSION=`python3.6 -c 'import sys; version=sys.version_info[:3]; print("{0}".format(version[1]))' || { echo "no py36"; }`
 
-# Check if the python command is python 3.5 or greater
-if [ "$PYTHON_VERSION_1" -eq "3" ]; then
-    if [ "$PYTHON_VERSION_2" -ge "5" ]; then
+if [ "$PYTHON_VERSION_1" -eq "3" ]; then # Python = 3
+    if [ "$PYTHON_VERSION_2" -eq "5" ]; then # Python = 3.5
+        if [ "$PYTHON_VERSION_3" -ge "3" ]; then # Python = 3.5.3
+            python run.py
+            exit
+        fi
+    elif [ "$PYTHON_VERSION_2" -eq "6" ]; then # Python = 3.6
         python run.py
-    else
-        echo "Your version of Python is lower than 3.5, please install a more recent version."
+        exit
     fi
-elif [ "$PYTHON3_VERSION" -ge "5" ]; then
-    python3 run.py
-# python3.5 and 3.6 check is to ensure that if the above two fail, the script
-# doesn't attempt to run something that doesn't exist
-elif [ "$PYTHON35_VERSION" -eq "5" ]; then
-    python3.5 run.py
-elif [ "$PYTHON36_VERSION" -eq "6" ]; then
-    python3.6 run.py
-else
-    echo "Your version of Python is lower than 3.5, please install a more recent version."
 fi
+
+if [ "$PYTHON3_VERSION_1" -eq "5" ]; then # Python3 = 3.5
+    if [ "$PYTHON3_VERSION_2" -ge "3" ]; then # Python3 > 3.5.3
+            python3 run.py
+            exit
+    fi
+fi
+
+if [ "$PYTHON3_VERSION_1" -eq "6" ]; then # Python3 = 3.6
+    python3 run.py
+    exit
+fi
+
+if [ "$PYTHON35_VERSION_2" -ge "3" ]; then # Python3.5 > 3.5.3
+    python3.5 run.py
+    exit
+fi
+
+if [ "$PYTHON36_VERSION" -eq "6" ]; then # Python3.6 = 3.6
+    python3.6 run.py
+    exit
+fi
+
+echo "You are running an unsupported Python version."
+echo "Please use a version of Python between 3.5.3 and 3.6"
