a7c8ed067de278b0625dd6d8f35f3653a0755b82
Jayden Bailey <jaydenkieran@gmail.com>
2018-08-27 10:53:50 +0100

    Bump version

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
 MAIN_VERSION = '1.9.8'
-SUB_VERSION = '-release'
+SUB_VERSION = '_1'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
