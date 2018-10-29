8575fc63568be815bda8743f80cc38979805a4f9
TheerapakG <theerapakg@gmail.com>
2018-10-29 10:59:15 +0700

    change self to bot

--- musicbot/commands/moderate.py
+++ musicbot/commands/moderate.py
@@ -4,7 +4,7 @@ from ..constructs import Response
 
 cog_name = 'moderate'
 
-async def cmd_karaoke(self, player, channel, author):
+async def cmd_karaoke(bot, player, channel, author):
     """
     Usage:
         {command_prefix}karaoke
@@ -15,7 +15,7 @@ async def cmd_karaoke(self, player, channel, author):
     player.karaoke_mode = not player.karaoke_mode
     return Response("\N{OK HAND SIGN} Karaoke mode is now " + ['disabled', 'enabled'][player.karaoke_mode], delete_after=15)
 
-async def cmd_blacklist(self, message, user_mentions, option, something):
+async def cmd_blacklist(bot, message, user_mentions, option, something):
     """
     Usage:
         {command_prefix}blacklist [ + | - | add | remove ] @UserName [@UserName2 ...]
@@ -29,35 +29,35 @@ async def cmd_blacklist(self, message, user_mentions, option, something):
 
     if option not in ['+', '-', 'add', 'remove']:
         raise exceptions.CommandError(
-            self.str.get('cmd-blacklist-invalid', 'Invalid option "{0}" specified, use +, -, add, or remove').format(option), expire_in=20
+            bot.str.get('cmd-blacklist-invalid', 'Invalid option "{0}" specified, use +, -, add, or remove').format(option), expire_in=20
         )
 
     for user in user_mentions.copy():
-        if user.id == self.config.owner_id:
+        if user.id == bot.config.owner_id:
             print("[Commands:Blacklist] The owner cannot be blacklisted.")
             user_mentions.remove(user)
 
-    old_len = len(self.blacklist)
+    old_len = len(bot.blacklist)
 
     if option in ['+', 'add']:
-        self.blacklist.update(user.id for user in user_mentions)
+        bot.blacklist.update(user.id for user in user_mentions)
 
-        write_file(self.config.blacklist_file, self.blacklist)
+        write_file(bot.config.blacklist_file, bot.blacklist)
 
         return Response(
-            self.str.get('cmd-blacklist-added', '{0} users have been added to the blacklist').format(len(self.blacklist) - old_len),
+            bot.str.get('cmd-blacklist-added', '{0} users have been added to the blacklist').format(len(bot.blacklist) - old_len),
             reply=True, delete_after=10
         )
 
     else:
-        if self.blacklist.isdisjoint(user.id for user in user_mentions):
-            return Response(self.str.get('cmd-blacklist-none', 'None of those users are in the blacklist.'), reply=True, delete_after=10)
+        if bot.blacklist.isdisjoint(user.id for user in user_mentions):
+            return Response(bot.str.get('cmd-blacklist-none', 'None of those users are in the blacklist.'), reply=True, delete_after=10)
 
         else:
-            self.blacklist.difference_update(user.id for user in user_mentions)
-            write_file(self.config.blacklist_file, self.blacklist)
+            bot.blacklist.difference_update(user.id for user in user_mentions)
+            write_file(bot.config.blacklist_file, bot.blacklist)
 
             return Response(
-                self.str.get('cmd-blacklist-removed', '{0} users have been removed from the blacklist').format(old_len - len(self.blacklist)),
+                bot.str.get('cmd-blacklist-removed', '{0} users have been removed from the blacklist').format(old_len - len(bot.blacklist)),
                 reply=True, delete_after=10
             )
