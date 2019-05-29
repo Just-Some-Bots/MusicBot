d70b9ec6f8aacaac26593857a4a142cc056d922f
TheerapakG <theerapakg@gmail.com>
2019-05-29 18:46:18 +0700

    Super annoyed by this

--- musicbot/player.py
+++ musicbot/player.py
@@ -223,7 +223,7 @@ class MusicPlayer(EventEmitter, Serializable):
                             break
                         except PermissionError as e:
                             if e.winerror == 32:  # File is in use
-                                log.error('Can\'t delete file, it is currently in use: {0}').format(filename)
+                                log.error('Can\'t delete file, it is currently in use: {0}'.format(filename))
                         except FileNotFoundError:
                             log.debug('Could not find delete {} as it was not found. Skipping.'.format(filename), exc_info=True)
                             break
