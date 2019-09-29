cb33cb52b17a2e072f33914310c8d7409e1db170
Winding <10997628+Winding6636@users.noreply.github.com>
2019-09-29 19:57:38 +0900

    fuckfix
    あほたれ

--- musicbot/downloader.py
+++ musicbot/downloader.py
@@ -20,7 +20,6 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',
-    'usenetrc': True
     'usenetrc': True,
     'retries': '10',
     'fragment-retries': '10'
