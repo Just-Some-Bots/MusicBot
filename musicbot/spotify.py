d8feba26849d0d9e73f10651db3fc60572e1ece5
AmiT177 <8685831+AmiT177@users.noreply.github.com>
2020-03-07 21:00:02 +0200

    Update spotify's endpoint

--- musicbot/spotify.py
+++ musicbot/spotify.py
@@ -95,12 +95,13 @@ class Spotify:
 
     async def request_guest_token(self):
         """Obtains a web player token from Spotify and returns it"""
-        async with self.aiosession.get("https://open.spotify.com/access_token?reason=transport&productType=web_player") as r:
+        async with self.aiosession.get("https://open.spotify.com/get_access_token?reason=transport&productType=web_player") as r:
             if r.status != 200:
                 try:
                     raise SpotifyError('Issue generating guest token: [{0.status}] {1}'.format(r, await r.json()))
                 except aiohttp.client_exceptions.ContentTypeError as e:
                     raise SpotifyError('Issue generating guest token: [{0.status}] {1}'.format(r, e))
+            return await r.json()
 
     def _make_token_auth(self, client_id, client_secret):
         auth_header = base64.b64encode((client_id + ':' + client_secret).encode('ascii'))
