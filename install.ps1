8a951ec6308048fa342e75ae64760e5b4287ba6b
TheerapakG <theerapakg@gmail.com>
2020-03-22 19:46:32 +0700

    Now with config

--- install.ps1
+++ install.ps1
@@ -15,6 +15,10 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 }
 
+# -----------------------------------------------------CONSTANTS-------------------------------------------------------
+
+$DEFAULT_URL_BASE = "https://discordapp.com/api"
+
 # ----------------------------------------------INSTALLING DEPENDENCIES------------------------------------------------
 
 # We will be using chocolatey to aid our installation
@@ -94,6 +98,8 @@ else
 Invoke-Expression "git clone https://github.com/Just-Some-Bots/MusicBot.git MusicBot -b $branch"
 Invoke-Expression "cd MusicBot"
 
+# --------------------------------------------INSTALL PYTHON DEPENDENCIES----------------------------------------------
+
 if (Get-Command "python" -errorAction SilentlyContinue)
 {
     $pythonver = @()
@@ -134,8 +140,76 @@ if($LastExitCode -eq 0)
 }
 
 Invoke-Expression "$PYTHON -m pip install --upgrade -r requirements.txt"
+
+# -------------------------------------------------CONFIGURE THE BOT---------------------------------------------------
 Copy-Item ".\config\example_options.ini" -Destination ".\config\options.ini"
 
-# Still too lazy to do the rest
-"Configure the bot by following this guide: https://just-some-bots.github.io/MusicBot/using/configuration/"
-"Then use run.bat to run the bot"
+# GET AND VERIFY TOKEN
+""
+"Please enter your bot token. This can be found in your discordapp developer page." 
+$token = Read-Host "Enter Token:" -AsSecureString
+$token_plain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($token))
+$header = @{
+    "Authorization" = "Bot $token_plain"
+    "Content-Type" = "application/json"
+}
+$result = Invoke-WebRequest -Headers $header -Method "GET" -Uri "$DEFAULT_URL_BASE/users/@me"
+$result_code = $result.StatusCode
+$result_content = $result.Content
+if (!($result_code -eq 200))
+{
+    "Error getting user profile, is the token correct? ($result_code $result_content)"
+    Return
+}
+$result_object = ConvertFrom-Json -InputObject $result_content
+# Cause whoever wrote ConvertFrom-Json cmdlet was insane and use some strange data type instead
+$result_table = @{}
+$result_object.parameters | Get-Member -MemberType NoteProperty | ForEach-Object{
+    $result_table += @{
+        $_.name = $result_object.parameters."$($_.name)" | Select-Object -ExpandProperty Value
+    }
+}
+$result_table += @{"token" = $token_plain}
+$config = (Get-Content -Path ".\config\options.ini") -creplace "bot_token", $token_plain
+
+# GET PREFIX
+$cprefix = Read-Host "Would you like to change the command prefix? [N/y]: "
+if($cprefix -eq "Y" -or $cprefix -eq "y")
+{
+    "Please enter the prefix you'd like for your bot."
+    $prefix = Read-Host "This is what comes before all commands. The default is [!] "
+    $config = $config -creplace "CommandPrefix = !", "CommandPrefix = $prefix"
+}
+else
+{
+    "Using default prefix [!]"
+}
+
+# GET OWNER
+$cowner = Read-Host "Would you like to automatically get the owner ID from the OAuth application? [Y/n]: "
+if($cowner -eq "N" -or $cowner -eq "n")
+{
+    $owner = Read-Host "Please enter the owner ID. "
+    $config = $config -creplace "OwnerID = auto", "OwnerID = $prefix"
+}
+else
+{
+    "Getting owner ID from OAuth application..."
+}
+
+# GET AP
+$cap = Read-Host "Would you like to enable the autoplaylist? [Y/n] "
+if($cap -eq "N" -or $cap -eq "n")
+{
+    $config = $config -creplace "UseAutoPlaylist = yes", "UseAutoPlaylist = no"
+    "Autoplaylist disabled"
+}
+else
+{
+    "Autoplaylist enabled"
+}
+
+"Saving your config..."
+Set-Content -Path ".\config\options.ini" -Value $config
+
+"You can now use run.bat to run the bot"
