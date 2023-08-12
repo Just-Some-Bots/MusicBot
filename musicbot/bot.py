daa73be5681abe541aef9fb6460eeea777efcddf
BabyBoySnow <earlSweatShirt1124@gmail.com>
2023-08-11 20:31:11 -0500

    Fix
    Fixes the summon message being displayed every time the play command is used. I tested best I could. Everything appears to be working as expected.

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -1689,7 +1689,7 @@ class MusicBot(discord.Client):
     ):
         player = _player if _player else None
 
-        if permissions.summonplay:
+        if permissions.summonplay and not player:
             voice_channel = author.voice.channel if author.voice else None
             response = await self.cmd_summon(
                 channel, channel.guild, author, voice_channel
