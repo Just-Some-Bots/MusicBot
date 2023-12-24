5cdf1a3da388ffbbb778d652534e67f6144dfeff
itsTheFae <itsthefae@gmail.com>
2023-12-23 20:47:18 -0800

    run black on dev merge.  grumble grumble

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -3727,7 +3727,6 @@ class MusicBot(discord.Client):
                 and not permissions.skiplooped
                 and player.repeatsong
             ):
-                
                 raise exceptions.PermissionsError(
                     self.str.get(
                         "cmd-skip-force-noperms-looped-song",
