dbdf5e7d57f0b1190cef25130c2517ef7823e7ab
itsTheFae <itsthefae@gmail.com>
2024-05-14 07:19:55 -0700

    Update install.bat to be more informative and less lazy.

--- install.bat
+++ install.bat
@@ -2,7 +2,15 @@
 
 CHCP 65001 > NUL
 
-REM --> This script only exists to easily launch the PowerShell script.
+REM --> The install.bat script only exists to easily launch the PowerShell script.
 REM --> By default powershell might error about running unsigned scripts...
 
-powershell.exe -noprofile -executionpolicy bypass -file install.ps1
+CD "%~dp0"
+
+SET InstFile="%~dp0%\install.ps1"
+IF exist %InstFile% (
+    powershell.exe -noprofile -executionpolicy bypass -file "%InstFile%"
+) ELSE (
+    echo Could not locate install.ps1
+    echo Please ensure it is available to continue the automatic install.
+)
