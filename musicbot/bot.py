3f1fcc7f8817a5358cbfb2b0390849964aad61bd
itsTheFae <itsthefae@gmail.com>
2024-09-05 15:37:14 -0700

    Fix playnext not respecting playlists

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -4134,7 +4134,7 @@ class MusicBot(discord.Client):
                     info,
                     channel=channel,
                     author=author,
-                    head=False,
+                    head=head,
                     ignore_video_id=ignore_video_id,
                 )
 
