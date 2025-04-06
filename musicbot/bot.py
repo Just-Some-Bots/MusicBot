473be0d4e5bb42589f7f3a56e3422f83526650d5
Fae <itsthefae@gmail.com>
2025-04-06 04:48:46 -0700

    Try to fix queue message being too large.

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -6340,6 +6340,7 @@ class MusicBot(discord.Client):
 
         # add the tracks to the embed fields
         tracks_list = ""
+        tracks_per_page = 0
         queue_segment = list(player.playlist.entries)[start_index:end_index]
         for idx, item in enumerate(queue_segment, starting_at):
             if item == player.current_entry:
@@ -6351,12 +6352,31 @@ class MusicBot(discord.Client):
             if item.channel and item.author:
                 added_by = item.author.name
 
-            tracks_list += _D(
+            # shorten the titles to get more tracks in the list.
+            title = item.title
+            if len(item.title) > 40:
+                title = item.title[:40] + " ..."
+
+            next_track_list = _D(
                 "**Entry #%(index)s:**"
                 "Title: `%(title)s`\n"
                 "Added by: `%(user)s`\n\n",
                 ssd_,
-            ) % {"index": idx, "title": _D(item.title, ssd_), "user": added_by}
+            ) % {"index": idx, "title": _D(title, ssd_), "user": added_by}
+            # We limit the track list, and leave extra space for the rest of the description text.
+            if (len(tracks_list) + len(next_track_list)) < 3840:
+                tracks_per_page += 1
+                tracks_list += next_track_list
+
+        if (
+            self.config.queue_length > tracks_per_page
+            and total_entry_count > self.config.queue_length
+        ):
+            log.warning(
+                "You may have QueueLength set too high! "
+                "The setting is %(option)d but we could only list %(count)d tracks.",
+                {"option": self.config.queue_length, "count": tracks_per_page},
+            )
 
         embed = Response(
             _D(
